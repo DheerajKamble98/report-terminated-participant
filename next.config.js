c71f1462bf2f74e6c757d8ff0d88b6ac86ad718a
DheerajKamble98 <www.breakkebaad@gmail.com>
2023-05-28 18:31:26 +0530

    report2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
 
-module.exports = nextConfig
+module.exports = {
+  webpack(config, options) {
+    if (!options.isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: 'reportTerminatedParticipant',
+          filename: 'static/chunks/remoteEntry.js',
+          remotes: {
+            home: `home@http://localhost:3000/_next/static/chunks/remoteEntry.js`,
+            reportTerminatedParticipant: 'reportTerminatedParticipant@http://localhost:3001/_next/static/chunks/remoteEntry.js',
+            reportLoginActivity: 'reportLoginActivity@http://localhost:3002/_next/static/chunks/remoteEntry.js',
+          },
+          exposes: {
+            './reportTerminatedParticipant': './pages/index.tsx',
+            './pages-map': './pages-map.tsx',
+          },
+          shared: {},
+        }),
+      );
+    }
+
+    return config;
+  },
+};
